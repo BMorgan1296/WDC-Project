8335cfdd30397c2cebabeea10ce869d5797b7b55
Daniel-ABK <33300406+Daniel-ABK@users.noreply.github.com>
2018-04-12 19:43:32 +0930

    Jquery Landing
    Search and currency

---
+++ scripts/landing.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    //Words shown next to search-bar
+    var searchWords = ["see","experience","find","explore","go"];
+    var currencies = ["AUD","CAD","USD","EUR"]
+  
+    updateCurrency("AUD");
+    
+    //Animate words next to search-bar
+    var textCount = Math.floor(Math.random()*searchWords.length);
+    textChange((textCount-1)%searchWords.length);
+    var textEnd = textCount+searchWords.length-1;
+    var textChanger = setInterval(function(){
+        textChange(textCount%searchWords.length);
+        textCount++;
+        if(textEnd==textCount){
+            clearInterval(textChanger);
+        }
+    }, 1500);
+    function textChange(index){
+        $("#searchText").fadeOut(function() {
+            $(this).text(searchWords[index]).fadeIn();
+        });
+    }
+    
+    //on-clicks
+    $("#currency").on("click",function(){
+        $("#currencyDrop").slideDown();
+    })
+    
+    $(document).on("click",".fixedPopDownItem",function(){
+        var val = $(this).attr('value');
+        updateCurrency(val);
+        $("#currencyText").text(val)
+    })
+    
+    //off-clicks
+    $(document).click(function(event){
+        var id = event.target.id;
+        if((id!='currency'&&id!="currencyText")&&$("#currencyDrop").css('display') == 'block') {
+            $("#currencyDrop").slideUp();
+        }   
+    })
+    
+    function updateCurrency(current){
+        $("#currencyDrop").empty();
+        for(var i = 0;i<currencies.length;i++){
+            if(current!=currencies[i]){
+                $("#currencyDrop").append("<div class='fixedPopDownItem' value='"+currencies[i]+"'>"+currencies[i]+"</div>");
+            }
+        }
+    }
+});
