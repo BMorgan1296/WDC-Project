7d5b6638c67af50f877adb7df0fc642ec12994a7
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2018-05-27 01:16:36 +0930

    added mysql files

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
+var mysql = require('mysql');
+var dbConnectionPool = mysql.createPool({host: 'localhost', user: 'root', password: 'password', database: 'PhoneCompany'});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,6 +27,12 @@ app.use(session
 	resave: true,
 	saveUninitialized: true
 }));
+
+app.use(function(req, res, next)
+{ 
+  req.pool = dbConnectionPool;
+  next(); 
+}); 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
